f764f0c4f0718d2313310400bbe6c578df7e370b
user <>
2022-07-16 17:37:36 +0200

    changes

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-function add_project(name, img_src, gif_src, description_text, link) {
+function add_project(name, img_src, gif_src, link, description_text) {
     const div_project = document.createElement("div");
     div_project.setAttribute("class", "project");
+    
+    const title_project = document.createElement("h2");
+    title_project.innerText = name;
 
     const project_link = document.createElement("a");
     project_link.setAttribute("href", link);
@@ -11,12 +14,13 @@ function add_project(name, img_src, gif_src, description_text, link) {
     if (gif_src) {
         img.setAttribute("src", gif_src);
     }
-    img.setAttribute("width", "400px");
+    img.setAttribute("width", "100%");
     img.setAttribute("height", "200px");
 
     const description = document.createElement("p");
     description.innerText = description_text;
     
+    div_project.append(title_project);
     project_link.append(img);
     div_project.append(project_link);
     div_project.append(description);
